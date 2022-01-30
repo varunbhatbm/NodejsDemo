7ceb83c6fb50b0eaa8849a17d8e27cf4776a9458
bhatbm <varun.bhat_b_m@nokia.com>
2022-01-30 14:30:52 +0530

    DB deps added

--- app/index-db.js
+++ app/index-db.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app= express();
-var mysql =  require('mysql');
+var mysql =  require("mysql");
 var con = mysql.createConnection({ host: process.env.MYSQL_HOST, user: process.env.MYSQL_USER, password: process.env.MYSQL_PASSWORD, database: process.env.MYSQL_DATABASE});
 
 con.connect(function(err) {
